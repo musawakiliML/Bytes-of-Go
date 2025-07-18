bf95bad01b2b7e6eca2fe949b2dc868663e85c62
Musa Adamu Wakili <musawakiliml@hotmail.com>
2025-07-18 01:17:57 +0100

    Update: Add form handler function and hello handler

--- Projects/go-server/main.go
+++ Projects/go-server/main.go
@@ -4,9 +4,56 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
 )
 
-func main(){
-	
+// Form Hanlder function
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	// Check if the request method is POST
+	if r.Method != "POST" {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm  error %v", err)
+		return
+	}
+
+	fmt.Fprintf(w, "Post Request Successfull!!\n")
+	name := r.FormValue("name")
+	email := r.FormValue("email")
+
+	fmt.Fprintf(w, "Name: %s\n", name)
+	fmt.Fprintf(w, "Email Address: %s\n", email)
+}
+
+// Hello Handler function
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 Not Found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+// A simple web server that serves static files from the "static" directory
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting Server At port 8080\n")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+
 }
